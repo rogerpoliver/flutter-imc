f6cbcd0f0bd6d19c375c0275bd7a625ce3d7a2e5
Roger Oliveira <rogerpolvr@gmail.com>
2019-10-28 23:38:11 -0300

    incluindo as validações nos campos

--- lib/main.dart
+++ lib/main.dart
@@ -15,44 +15,54 @@ class _HomeState extends State<Home> {
   TextEditingController weightController = TextEditingController();
   TextEditingController heightController = TextEditingController();
 
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   String _infoText = "Please fill out the form";
 
   void _resetFields() {
     weightController.text = "";
     heightController.text = "";
-    _infoText = "";
+    setState(() {
+      _infoText = "Please fill out the form";
+    });
   }
 
   void _calculate() {
-   setState(() {
-     double weight = double.parse(weightController.text);
-     double height = double.parse(heightController.text) / 100;
-     double imc = weight / (height * height);
-     debugPrint(imc.toString());
-     if (imc < 15) {
-       _infoText = "${imc.toStringAsPrecision(4)} - Very severely underweight";
-     }else if(imc <= 16){
-       _infoText = "${imc.toStringAsPrecision(4)} - Severely underweight";
-     }else if(imc <= 18.5){
-       _infoText = "${imc.toStringAsPrecision(4)} - Underweight";
-     }else if(imc <= 25){
-       _infoText = "${imc.toStringAsPrecision(4)} - Normal (healthy weight)";
-     }else if(imc <= 30){
-       _infoText = "${imc.toStringAsPrecision(4)} - Overweight";
-     }else if(imc <= 35){
-       _infoText = "${imc.toStringAsPrecision(4)} - Obese Class I (Moderately obese)";
-     }else if(imc <= 40){
-       _infoText = "${imc.toStringAsPrecision(4)} - Obese Class II (Severely obese)";
-     }else if(imc <= 45){
-       _infoText = "${imc.toStringAsPrecision(4)} - Obese Class III (Very severely obese)";
-     }else if(imc <= 50){
-       _infoText = "${imc.toStringAsPrecision(4)} - Obese Class IV (Morbidly obese)";
-     }else if(imc <= 60){
-       _infoText = "${imc.toStringAsPrecision(4)} - Obese Class V (Super obese)";
-     }else if(imc > 60){
-       _infoText = "${imc.toStringAsPrecision(4)} - Obese Class VI (Hyper obese)";
-     }
-   });
+    setState(() {
+      double weight = double.parse(weightController.text);
+      double height = double.parse(heightController.text) / 100;
+      double imc = weight / (height * height);
+      debugPrint(imc.toString());
+      if (imc < 15) {
+        _infoText = "${imc.toStringAsPrecision(4)} - Very severely underweight";
+      } else if (imc <= 16) {
+        _infoText = "${imc.toStringAsPrecision(4)} - Severely underweight";
+      } else if (imc <= 18.5) {
+        _infoText = "${imc.toStringAsPrecision(4)} - Underweight";
+      } else if (imc <= 25) {
+        _infoText = "${imc.toStringAsPrecision(4)} - Normal (healthy weight)";
+      } else if (imc <= 30) {
+        _infoText = "${imc.toStringAsPrecision(4)} - Overweight";
+      } else if (imc <= 35) {
+        _infoText =
+            "${imc.toStringAsPrecision(4)} - Obese Class I (Moderately obese)";
+      } else if (imc <= 40) {
+        _infoText =
+            "${imc.toStringAsPrecision(4)} - Obese Class II (Severely obese)";
+      } else if (imc <= 45) {
+        _infoText =
+            "${imc.toStringAsPrecision(4)} - Obese Class III (Very severely obese)";
+      } else if (imc <= 50) {
+        _infoText =
+            "${imc.toStringAsPrecision(4)} - Obese Class IV (Morbidly obese)";
+      } else if (imc <= 60) {
+        _infoText =
+            "${imc.toStringAsPrecision(4)} - Obese Class V (Super obese)";
+      } else if (imc > 60) {
+        _infoText =
+            "${imc.toStringAsPrecision(4)} - Obese Class VI (Hyper obese)";
+      }
+    });
   }
 
   @override
@@ -74,55 +84,74 @@ class _HomeState extends State<Home> {
         ),
         backgroundColor: Colors.white,
         body: SingleChildScrollView(
-            padding: EdgeInsets.all(20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                Icon(
-                  Icons.person,
-                  color: Colors.amber,
-                  size: 120,
-                ),
-                TextField(
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                      labelText: "Weight (Kg)",
-                      labelStyle:
-                          TextStyle(color: Colors.pinkAccent, fontSize: 25)),
-                  textAlign: TextAlign.center,
-                  style: TextStyle(color: Colors.amber, fontSize: 25),
-                  controller: weightController,
-                ),
-                TextField(
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                      labelText: "Height (Cm)",
-                      labelStyle:
-                          TextStyle(color: Colors.pinkAccent, fontSize: 25)),
-                  textAlign: TextAlign.center,
-                  style: TextStyle(color: Colors.amber, fontSize: 25),
-                  controller: heightController,
-                ),
-                Padding(
-                  padding: EdgeInsets.all(10),
-                  child: Container(
-                    height: 50,
-                    child: RaisedButton(
-                      onPressed: _calculate,
-                      child: Text(
-                        "Result",
-                        style: TextStyle(color: Colors.white, fontSize: 25),
+          padding: EdgeInsets.all(20),
+          child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Icon(
+                    Icons.person,
+                    color: Colors.amber,
+                    size: 120,
+                  ),
+                  TextFormField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                        labelText: "Weight (Kg)",
+                        labelStyle:
+                            TextStyle(color: Colors.pinkAccent, fontSize: 25)),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.amber, fontSize: 25),
+                    controller: weightController,
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please enter the value';
+                      }
+                      return null;
+                    },
+                  ),
+                  TextFormField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                        labelText: "Height (Cm)",
+                        labelStyle:
+                            TextStyle(color: Colors.pinkAccent, fontSize: 25)),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.amber, fontSize: 25),
+                    controller: heightController,
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please enter the value';
+                      }
+                      return null;
+                    },
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Container(
+                      height: 50,
+                      child: RaisedButton(
+                        onPressed: (){
+                          if(_formKey.currentState.validate()){
+                            _calculate();
+                          }
+                        },
+                        child: Text(
+                          "Result",
+                          style: TextStyle(color: Colors.white, fontSize: 25),
+                        ),
+                        color: Colors.pinkAccent,
                       ),
-                      color: Colors.pinkAccent,
                     ),
                   ),
-                ),
-                Text(
-                  "$_infoText",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(color: Colors.pinkAccent, fontSize: 25),
-                )
-              ],
-            )));
+                  Text(
+                    "$_infoText",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.pinkAccent, fontSize: 25),
+                  )
+                ],
+              )),
+        ));
   }
 }
